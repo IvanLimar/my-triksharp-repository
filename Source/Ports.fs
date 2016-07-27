4c544e56c77f4feb4f96f03f21df48941001f988
Ivan Limar <limvan@mail.ru>
2016-07-27 07:32:10 +0300

    Updated encoder ports and servo ports, added sonar ports

--- Source/Ports.fs
+++ Source/Ports.fs
@@ -2,14 +2,14 @@
     type IEncoderPort =
         abstract member I2CNumber : int
     ///<summary>Type representing Encoders ports</summary>
-    type EncoderPort  = B1| B2 | B3 | B4 with 
+    type EncoderPort  = B1 | B2 | B3 | B4 | E1 | E2 | E3 | E4 with 
         interface IEncoderPort with
             member self.I2CNumber = 
                 match self with 
-                    | B1 -> 0x30
-                    | B2 -> 0x31 
-                    | B4 -> 0x32 
-                    | B3 -> 0x33
+                    | B1 | E1 -> 0x30
+                    | B2 | E2 -> 0x31 
+                    | B4 | E3 -> 0x32 
+                    | B3 | E4 -> 0x33
     
     type IMotorPort =
         abstract member I2CNumber : int
@@ -25,16 +25,16 @@
     
     type IServoPort =
         abstract member Path : string
-    type ServoPort = E1 | E2 | E3 | C1 | C2 | C3 with
+    type ServoPort = E1 | E2 | E3 | C1 | C2 | C3 | S1 | S2 | S3 | S4 | S5 | S6 with
         interface IServoPort with
             member self.Path = 
                 match self with 
-                | E1 -> "/sys/class/pwm/ehrpwm.1:1" 
-                | E2 -> "/sys/class/pwm/ehrpwm.1:0" 
-                | E3 -> "/sys/class/pwm/ehrpwm.0:1"             
-                | C1 -> "/sys/class/pwm/ecap.0" 
-                | C2 -> "/sys/class/pwm/ecap.1" 
-                | C3 -> "/sys/class/pwm/ecap.2" 
+                | E1 | S6 -> "/sys/class/pwm/ehrpwm.1:1" 
+                | E2 | S5 -> "/sys/class/pwm/ehrpwm.1:0" 
+                | E3 | S4 -> "/sys/class/pwm/ehrpwm.0:1"             
+                | C1 | S3 -> "/sys/class/pwm/ecap.0" 
+                | C2 | S2 -> "/sys/class/pwm/ecap.1" 
+                | C3 | S1 -> "/sys/class/pwm/ecap.2" 
     
     type ISensorPort =
         abstract member I2CNumber : int
@@ -49,5 +49,16 @@
                 | A4 -> 0x22
                 | A5 -> 0x21
                 | A6 -> 0x20
+                
+    type ISonarPort =
+        abstract member EventFile : string
+        
+    type SonarPort = D1 | D2 with
+        interface ISonarPort with
+            member self.EventFile =
+                match self with
+                | D1 -> "/dev/input/by-path/platform-trik_jd1-event"
+                | D2 -> "/dev/input/by-path/platform-trik_jd2-event"
+       
 
     type VideoSource = USB = 0 | VP1 = 1 | VP2 = 2
